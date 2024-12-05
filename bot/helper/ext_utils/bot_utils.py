c4c0798090d092a07dfea71706bc27baf371c9a8
pkbotzs <165777512+pkbotzs@users.noreply.github.com>
2024-12-05 12:24:00 -0800

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -248,7 +248,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = "<b>Powered by Aeon</b>\n\n"
+    msg = "<b>Powered by TamilX</b>\n\n"
     button = None
     tasks = len(download_dict)
     current_time = get_readable_time(time() - bot_start_time)
